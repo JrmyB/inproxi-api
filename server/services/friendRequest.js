bce90e0cfa09883d6c1a83d71c65420753cc43c5
JrmyB <bljrmy@gmail.com>
2017-09-05 20:55:00 +0200

    fix typing errors

--- server/services/friendRequest.js
+++ server/services/friendRequest.js
@@ -20,7 +20,7 @@ function update(req, res) {
     if (fr === null) return res.status(404).send({ message: 'Friend request not found.' });
 
     methods.updateFriendRequest(fr, req.body.status, err => err
-				? res.status(500).send({ message: 'Internal server error.'});
+				? res.status(500).send({ message: 'Internal server error.'})
 				: res.sendStatus(200))
   });
 }

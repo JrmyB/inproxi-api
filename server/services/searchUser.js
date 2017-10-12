b2e78a61fbf7723aa30fdabf8225dec13784a477
JrmyB <bljrmy@gmail.com>
2017-10-12 19:32:08 +0200

    Fix deprecated warning from express: req.param(name)

--- server/services/searchUser.js
+++ server/services/searchUser.js
@@ -3,10 +3,10 @@
 const userMethods = require('../models/user/methods')
 
 const searchUser = (req, res) => {
-  if (!req.param('first_name') && !req.param('last_name'))
+  if (!req.query.first_name && !req.query.last_name)
     return res.status(422).json({ message: 'Required field(s) missing.'})
 
-  userMethods.searchUser(req.param('first_name'), req.param('last_name'))
+  userMethods.searchUser(req.query.first_name, req.query.last_name)
     .then(user => {
       if (user === null) return res.status(404).send({ message: 'User not found.' })
       res.status(200).json(user)

12830224487960fc45ede378aa0bf19b9bb598a2
JrmyB <bljrmy@gmail.com>
2017-12-09 13:21:34 +0100

    edit create message method

--- server/models/message/methods.js
+++ server/models/message/methods.js
@@ -5,6 +5,12 @@ const Message = require('./')
 const createMessage = data => new Promise((resolve, reject) => {
   const message = new Message(data)
 
+  const message = new Message({
+    conversation_id: data.conversation_id,
+    content: data.content,
+    author: data.author
+  })
+  
   message.save()
     .then(message => resolve(message))
     .catch(err => reject(err))
